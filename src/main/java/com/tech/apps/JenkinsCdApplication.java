860d77feeddbadcb4575f3bb0e3a3bc50f68c15d
Manikanta-12345 <suryamutyala757@gmail.com>
2021-08-23 21:11:39 +0530

    second change

--- src/main/java/com/tech/apps/JenkinsCdApplication.java
+++ src/main/java/com/tech/apps/JenkinsCdApplication.java
@@ -2,9 +2,10 @@ package com.tech.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsCdApplication {
+public class JenkinsCdApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCdApplication.class, args);
