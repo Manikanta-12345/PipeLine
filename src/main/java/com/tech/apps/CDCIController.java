2353eea3125ca6e7656e5c6ec8686d3defd9e544
Manikanta-12345 <suryamutyala757@gmail.com>
2021-08-23 21:41:19 +0530

    second change

--- src/main/java/com/tech/apps/CDCIController.java
+++ src/main/java/com/tech/apps/CDCIController.java
@@ -9,6 +9,6 @@ public class CDCIController {
 
 	@RequestMapping(value = "/jenkins",method = RequestMethod.GET)
 	public String welcomeJenkise() {
-		return "Jenkins CD/CI First PipeLine";
+		return "Jenkins CD/CI First PipeLine Automation";
 	}
 }
